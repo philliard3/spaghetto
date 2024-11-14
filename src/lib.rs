9d31b3d19acf36a0aa99710a9cc53a1c83275dff
Phillip Hilliard <pdh@seas.upenn.edu>
2024-11-14 18:26:13 -0500

    updated docs to use a more accurate example for what would cause VecDeque to lose its contiguousness

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,25 @@
 //! - A [`DeVec`] can be used as a double-ended queue, but with the added benefit of being able to get a single contiguous slice of the entire structure.
 //! ```
 //! use spaghetto::DeVec;
-//!
-//! let mut devec = DeVec::new();
+//! let mut devec = DeVec::with_capacity(5);
 //! devec.push_back(1);
 //! devec.push_front(2);
 //! devec.push_back(3);
-//!
 //! assert_eq!(devec.pop_front(), Some(2));
-//!
-//! // now let's overload one side to force it to reallocate. With a VecDeque this would no longer be a contiguous allocation.
-//! for i in 0..100 {
+//! devec.pop_front();
+//! // Now let's overload one side.
+//! // This forces a DeVec to reallocate in order to keep the elements contiguous.
+//! // With a VecDeque this would no longer be a contiguous allocation
+//! // because we moved the front of the queue into the middle of the buffer
+//! // and pushed it out past the end of the buffer.
+//! // It would need to wrap around.
+//! for i in 0..3 {
 //!    devec.push_back(i);
 //! }
-//!
+//! 
 //! // we can get a single contiguous slice of the entire DeVec without having to shift elements.
-//! let slice = devec.as_slice();
+//! let no_mutation = &devec;
+//! let slice = no_mutation.as_slice();
 //! ```
 //!
 //! - A [`DeString`] can be used as a double-ended string, and because of this, we can efficiently remove extra whitespace from either side, mutating in place and maintining a single contiguous string slice without the cost of shifting like with a String.
